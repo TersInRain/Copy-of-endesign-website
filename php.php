10b3f18be20ae01a4ec8e7fd1ca4cc358b467ea8
Artem Petrov <poupkin111@mail.ru>
2021-09-15 16:50:59 +0300

    change php

--- php.php
+++ php.php
@@ -1,50 +1,68 @@
 
 <?php
 // Получаем значения переменных из пришедших данных
-$text = $_POST['text'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
-$pol = $_POST['pol'];
-$usl = $_POST['usl'];
-$comment = $_POST['comment'];
+error_reporting( E_ERROR );
+if (isset($_POST['name'])){$name= $_POST['name'];
+if ($name == ''){unset($name);}}
 
-$name = htmlspecialchars($name);
+if (isset($_POST['email'])){$email= $_POST['email'];
+if ($email == ''){unset($email);}}
+
+if (isset($_POST['tel'])){$tel= $_POST['tel'];
+if ($tel == ''){unset($tel);}}
+
+if (isset($_POST['pol'])){$pol= $_POST['pol'];
+if ($pol == ''){unset($pol);}}
+
+if (isset($_POST['usl'])){$usl= $_POST['usl'];
+if ($usl == ''){unset($usl);}}
+
+if (isset($_POST['comment'])){$comment= $_POST['comment'];
+if ($comment == ''){unset($comment);}}
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $tel = $_POST['tel'];
+// $pol = $_POST['pol'];
+// $usl = $_POST['usl'];
+// $comment = $_POST['comment'];
+
+
+if (isset($name) ){
 $email = htmlspecialchars($email);
 $tel = htmlspecialchars($tel);
 $pol = htmlspecialchars($pol);
 $usl = htmlspecialchars($usl);
-$comment = trim($comment);
+$comment = trim($comment);}
 
- 
-$name = urldecode($name);
+if (isset($name) ) {
 $email = urldecode($email);
 $tel = urldecode($tel);
 $pol = urldecode($pol);
 $usl = urldecode($usl);
-$comment = trim($comment);
+$comment = trim($comment);}
 
-$name = trim($name);
+if (isset($name) ){
 $email = trim($email);
 $tel = trim($tel);
 $pol = trim($pol);
 $usl = trim($usl);
-$comment = trim($comment);
+$comment = trim($comment);}
 
 
-if (email("admin@mail.ru",
-	"Новое письмо с сайта",
-	"text: ".$text."\n".
-	"email: ".$email."\n".
-	"tel: ".$tel."\n".
-	"pol: ".$pol."\n".
-	"usl: ".$usl."\n".
-	"comment: ".$comment."\n".
-	"From: no-reply@mydomain.ru \r\n")
-	
-	) {
-		echo('Письмо ');
-	}
-	else {
-		echo('Есть ');
-	}
+
+$address="poupkin111@mail.ru";
+// текст письма 
+$note_message="Имя : $name \r\n Email : $email \r\n Телефон : \r\n Пол : $pol \r\n Улуги : $usl \r\n Комментарий : $message";
+
+if (isset($name)  &&  isset ($email) ) {
+mail($address,$note_message,"Content-type:message/plain; windows-1251"); 
+// сообщение после отправки формы
+    
+echo "Уважаемый(ая) $name</b> Ваше письмо отправленно успешно <br>Вам скоро ответят на почту  $email</b>.";
+}
+else{
+    echo "что-то пошло не так";
+}
 ?>
+
+
